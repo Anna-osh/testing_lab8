a2fd241bae6922f03fda97d3f78798f40e1583aa
Anna <56040049+Anna-osh@users.noreply.github.com>
2021-11-21 21:27:01 +0300

    Update WebDriverDreamlinesTest.java

--- lab8/src/test/java/test/WebDriverDreamlinesTest.java
+++ lab8/src/test/java/test/WebDriverDreamlinesTest.java
@@ -17,11 +17,11 @@ public class WebDriverDreamlinesTest {
 
     @BeforeMethod (alwaysRun = true)
     public void browserSetup(){
-        /*ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
-        driver.manage().window().maximize();*/
-        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver = new ChromeDriver();
     }
 
     @Test
